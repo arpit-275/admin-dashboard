676779a3763c4f537eb6c203fb1053f66d1afc9f
arpit-275 <arpitrgupta27@gmail.com>
2022-12-05 15:20:04 +0530

    setup eslint config and basic folder structure

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './sidebar.scss';
+
+export default function Sidebar() {
+  return <div>Sidebar</div>;
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
+import './home.scss';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Sidebar />
+    </div>
+  );
+}
