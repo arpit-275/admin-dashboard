1e3dd0e802f712a5656b29ce97e75a3575b54b02
arpit-275 <arpitrgupta27@gmail.com>
2022-12-05 19:08:59 +0530

    Added widget component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import './home.scss';
+
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
-import './home.scss';
+import Widget from '../../components/widget/Widget';
 
 export default function Home() {
   return (
@@ -9,6 +11,12 @@ export default function Home() {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
